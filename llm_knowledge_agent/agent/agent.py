f0b83fa0a7d06cbd996b238e6eaee74a8dfb159b
kylestratis <kyle.stratis@gmail.com>
2023-05-08 23:25:57 -0400

    Some restructuring, placeholder text ingestor. Fixes #4

---
+++ llm_knowledge_agent/agent/agent.py
@@ -0,0 +1,3 @@
+def ingest_article(text: str) -> str:
+    # TODO do any text preprocessing here
+    return text
---
+++ llm_knowledge_agent/agent_runner.py
@@ -0,0 +1,4 @@
+from dotenv import dotenv_values
+import pinecone
+
+config = dotenv_values("../.env")
